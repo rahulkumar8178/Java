1c6242c2a601af77bb55f021707f70d1d5f01077
Rahul <princeroyce9199@gmail.com>
2023-02-14 22:49:32 +0530

    My First Commit

---
+++ Test.java
@@ -0,0 +1,23 @@
+public class Test {
+    public static void main(String[] args){
+    char x = 3;
+    switch (x)
+    {
+        case 1:
+            System.out.println(97);
+            break;
+
+        case 2:
+            System.out.println(99);
+            break;
+
+        case 3:
+            System.out.println('a');
+                      
+                 
+        default:
+            System.out.println("Default wala");
+          
+    }
+}
+}
